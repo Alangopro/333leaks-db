bb321127cd520feca17d54600f0f1599b7abca5a
Kamerzystanasyt <91615271+Alangopro@users.noreply.github.com>
2024-04-22 21:15:44 +0100

    Create searchpass.js

---
+++ pages/api/searchpass.js
@@ -0,0 +1,19 @@
+export default async function handler(req, res) {
+  const { nick } = req.query;
+
+  try {
+    const response = await fetch('https://333leaks.netlify.app/haslamc.txt');
+    const data = await response.text();
+
+    const foundLine = data.split('\n').find(line => line.includes(nick));
+
+    if (foundLine) {
+      res.status(200).json({ data: foundLine });
+    } else {
+      res.status(404).json({ error: 'Nick not found' });
+    }
+  } catch (error) {
+    console.error('Error fetching file:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+}
