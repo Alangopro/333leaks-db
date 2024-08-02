7d13f8f07b6a9429b83432704a69053719676c35
Kamerzystanasyt <91615271+Alangopro@users.noreply.github.com>
2024-08-02 18:00:21 +0200

    Update kangaroohub.js

--- pages/kangaroohub.js
+++ pages/kangaroohub.js
@@ -1,17 +1,13 @@
-import fs from 'fs';
-import path from 'path';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-const luaFilePath = path.join(__dirname, 'kangaroohub.lua');
-const fileContents = fs.readFileSync(luaFilePath, 'utf8');
-
-export default function Kangaroohub() {}
-
-export const getServerSideProps = async () => {
-  return {
-    props: {},
-    headers: {
-      'Content-Type': 'text/plain',
-    },
-    body: fileContents,
-  };
-};
+export default async function Kangaroohub(req: NextApiRequest, res: NextApiResponse) {
+  const url = 'https://333leaks.netlify.app/kangaroohub.lua';
+  try {
+    const response = await fetch(url);
+    const fileContents = await response.text();
+    return res.status(200).setHeader('Content-Type', 'text/plain').send(fileContents);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('Error fetching file contents');
+  }
+}
