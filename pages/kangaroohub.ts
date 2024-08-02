546de0807a1abdd90dc5d72a471fa99fe3073b38
Kamerzystanasyt <91615271+Alangopro@users.noreply.github.com>
2024-08-02 18:09:15 +0200

    Update kangaroohub.ts

--- pages/kangaroohub.ts
+++ pages/kangaroohub.ts
@@ -1,14 +1,15 @@
-import fetch from 'node-fetch';
+import Head from 'next/head';
 
 export default async function Kangaroohub() {
   const response = await fetch('https://333leaks.netlify.app/kangaroohub.lua');
   const fileContents = await response.text();
 
-  return {
-    props: {},
-    headers: {
-      'Content-Type': 'text/plain',
-    },
-    body: fileContents,
-  };
+  return (
+    <div>
+      <Head>
+        <meta httpEquiv="Content-Type" content="text/plain; charset=UTF-8" />
+      </Head>
+      <pre>{fileContents}</pre>
+    </div>
+  );
 }
