43c8889e838023779f66b6cfd9328d7ccb7afbe6
Kamerzystanasyt <91615271+Alangopro@users.noreply.github.com>
2024-08-02 17:29:24 +0200

    Update kangaroohub.js

--- pages/kangaroohub.js
+++ pages/kangaroohub.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-const filePath = path.join(process.cwd(), 'kangaroohub.lua');
+const filePath = path.join(process.cwd(), './kangaroohub.lua');
 
 export default function Kangaroohub() {}
 
